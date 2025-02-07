ef7b79aba1c200a3b678d0cf1c99e06eaed33fdb
Adam Nafie <adamnafie@gmail.com>
2025-02-07 22:08:14 +0100

    docker deploy working locally and on railway

--- scan2mail/settings.py
+++ scan2mail/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # The secret key
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
  
-DEBUG = False#bool(os.environ.get("DEBUG", default=0))
+DEBUG = True#bool(os.environ.get("DEBUG", default=0))
 
 # ~ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://scan2mail.up.railway.app']
 
 
 # Application definition
